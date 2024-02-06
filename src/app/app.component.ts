85a34de0b1aa052ac23d822a5982fc98cfb5ad42
LauraaPaiva <lauraapaiva94@gmail.com>
2024-02-05 21:18:20 -0300

    Entendendo componentes e estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { SecondComponentComponent } from "./components/second-component/second-component.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent]
+  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'hello';
