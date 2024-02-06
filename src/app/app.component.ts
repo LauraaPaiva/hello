83aff546de10aa7481a19666fc047fe9ced7f1d8
LauraaPaiva <lauraapaiva94@gmail.com>
2024-02-05 21:00:18 -0300

    Testes de estilo, links e segundo componete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { SecondComponentComponent } from "./components/second-component/second-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, FirstComponentComponent]
+  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent]
 })
 export class AppComponent {
   title = 'hello';
